b9c410fdada7a7782dcd5ff2de534130d34a02a7
ntra7289 <anthonytran0611@gmail.com>
2025-09-21 04:46:12 +1000

    update ui login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowRight, ChevronRight } from "lucide-react";
+import { ArrowRight, ChevronRight, ShuffleIcon, SquircleDashed } from "lucide-react";
 import { HomeMain } from "~/components/home/HomeMain";
 import { LoadingPage } from "~/components/LoadingPage";
 
@@ -27,7 +27,7 @@ const Home = () => {
         {(isSignedIn) ? (
           <HomeMain />
           ) : ( 
-          <div className="min-h-screen bg-gray-100">
+          <div className="min-h-screen bg-[#f7f7f0]">
             <div className="w-full h-10 bg-blue-50 flex items-center justify-center">
               Meet Omni, your AI collaborator for building custom apps.{" "}
               <Link href="/" className="ml-2 text-blue-600 font-semibold hover:underline" target="_blank">
@@ -35,7 +35,7 @@ const Home = () => {
               </Link>
               <ArrowRight className="text-blue-600" size={16} strokeWidth={2}/>
             </div>
-            <div className="h-20 flex items-center justify-between bg-gray-50 hover:bg-white transition-colors duration-300">
+            <div className="h-20 flex items-center justify-between bg-[#f7f7f0] hover:bg-white transition-colors duration-300 shadow-[0_8px_16px_rgba(0,0,0,0.03)]">
               <div className="pl-10 flex items-center">
                 <Link href="/">
                   <Image
@@ -73,10 +73,27 @@ const Home = () => {
               </div>
               
             </div>
+            <div className="pt-10 flex flex-col items-center text-[50px] leading-[1.2]">
+              <span>From idea to app in an instant</span>
+              <span>Build with AI that means business</span>
+              <div className="mt-10 h-55 w-195 bg-white rounded-3xl shadow-[0_2px_8px_rgba(0,0,0,0.06)] flex flex-col justify-between">
+                <span className="px-6 pt-8 text-[21px]">Create a CRM for my startup to track leads and customer relationships</span>
+                <div className="px-4 pb-5 flex flex-row justify-between">
+                  <button className="px-6 w-52 h-15 border border-gray-300 rounded-full hover:bg-gray-100 hover:cursor-pointer flex flex-row gap-4 items-center">
+                    <ShuffleIcon size={16} />
+                    <span className="text-[16px] font-semibold">New Suggestion</span>
+                  </button>
+                  <button className="pl-5 pr-2 w-40 h-15 bg-black rounded-full hover:cursor-pointer flex flex-row gap-2 items-center">
+                    <SquircleDashed className="text-white" size={20} />
+                    <span className="text-[17px] text-white font-semibold">Build it now</span>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
           )
         }
-        
+      
       </main>
     </>
   );
